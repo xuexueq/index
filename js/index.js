bf5e0e534cba471d631c32f9460e8c956f9c398e
xuekeai <1285958162@qq.com>
2017-10-21 11:28:21 +0800

    updata

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     speed:800,
-    autoplay : 2000,
-    // 分页器
-    // pagination: '.swiper-pagination',
-    
-    // 前进后退按钮
-    // nextButton: '.swiper-button-next',
-    // prevButton: '.swiper-button-prev',
-    
-  })        
+    autoplay : 2000    
+  }) 
+
+  $('.message-task').on('click',function(e){
+    e.stopPropagation();
+    var target=e.target;
+    var message=$(target).parent()
+    message.addClass('message-active')
+    message.siblings().removeClass('message-active')
+  })       
